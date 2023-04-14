1f66f5b5dd4177fe031677a7098c41b10cbbce83
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-04-14 15:45:22 +0300

    Create problem6_answers.py

---
+++ UNIT 2/Problem Set 2/problem6_answers.py
@@ -0,0 +1,78 @@
+# Problem 6-1
+# Examine showPlot1 in ps2.py, which takes in the parameters title, x_label, and y_label. 
+# Your job is to examine the code and figure out what the plot produced by the function tells you. 
+# Try calling showPlot1 with appropriate arguments to produce a few plots. 
+# Then, answer the following 3 questions.
+
+
+# 1. Which of the following would be the best title for the graph?
+
+# Percentage Of Room That A Robot Cleans
+# Time It Takes Two Robots To Clean 80% Of Variously Sized Rooms
+# Time It Takes Two Robots To Clean 80% Of Variously Shaped Rooms
+# Time It Takes 1 - 10 Robots To Clean 80% Of A Room
+# Percentage Of Variously Sized Rooms That A Robot Cleans
+# Percentage Of Variously Shaped Rooms That A Robot Cleans
+
+# ANSWER: Time It Takes 1 - 10 Robots To Clean 80% Of A Room
+
+
+# 2. Which of the following would be the best x-axis label for the graph?
+
+# Time-steps
+# Percentage Cleaned
+# Aspect Ratio
+# Number of Robots
+# Distance Travelled
+
+# ANSWER: Number of Robots
+
+# 3. Which of the following would be the best y-axis label for the graph?
+
+# Time-steps
+# Percentage Cleaned
+# Aspect Ratio
+# Number of Robots
+# Distance Travelled
+
+# ANSWER: Time-steps
+
+#----------------------------------------#
+#----------------------------------------#
+
+# Problem 6-2
+# Examine showPlot2 in ps2.py, which takes in the parameters title, x_label, and y_label. 
+# Your job is to examine the code and figure out what the plot produced by the function tells you. 
+# Try calling showPlot2 with appropriate arguments to produce a few plots. 
+# Then, answer the following 3 questions.
+
+# 1. Which of the following would be the best title for the graph?
+
+# Percentage Of Room That A Robot Cleans
+# Time It Takes Two Robots To Clean 80% Of Variously Sized Rooms
+# Time It Takes Two Robots To Clean 80% Of Variously Shaped Rooms
+# Time It Takes 1 - 10 Robots To Clean 80% Of A Room
+# Percentage Of Variously Sized Rooms That A Robot Cleans
+# Percentage Of Variously Shaped Rooms That A Robot Cleans
+
+# ANSWER: Time It Takes Two Robots To Clean 80% Of Variously Shaped Rooms
+
+# 2. Which of the following would be the best x-axis label for the graph?
+
+# Time-steps
+# Percentage Cleaned
+# Aspect Ratio
+# Number of Robots
+# Distance Travelled
+
+# ANSWER: Aspect Ratio
+
+# 3. Which of the following would be the best y-axis label for the graph?
+
+# Time-steps
+# Percentage Cleaned
+# Aspect Ratio
+# Number of Robots
+# Distance Travelled
+
+# ANSWER: Time-steps
