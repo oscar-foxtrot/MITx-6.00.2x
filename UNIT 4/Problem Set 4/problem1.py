fbe0d314d17dc2f17ed24155d0418ef2394ba785
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-05-06 09:16:32 +0300

    Create problem1.py

---
+++ UNIT 4/Problem Set 4/problem1.py
@@ -0,0 +1,22 @@
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+import numpy as np
+
+# Problem 1
+def generate_models(x, y, degs):
+    """
+    Generate regression models by fitting a polynomial for each degree in degs
+    to points (x, y).
+    Args:
+        x: a list with length N, representing the x-coords of N sample points
+        y: a list with length N, representing the y-coords of N sample points
+        degs: a list of degrees of the fitting polynomial
+    Returns:
+        a list of numpy arrays, where each array is a 1-d array of coefficients
+        that minimizes the squared error of the fitting polynomial
+    """
+    # TODO
+    res = []
+    for deg in degs:
+        res += [np.polyfit(x, y, deg)]
+    return res
