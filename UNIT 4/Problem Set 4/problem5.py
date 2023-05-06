217250745f1f0ec57f7b2b0dad644b54b57bc44c
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-05-06 13:10:46 +0300

    Create problem5.py

---
+++ UNIT 4/Problem Set 4/problem5.py
@@ -0,0 +1,15 @@
+# Problem 5
+# Which graph, choosing a specific day (Jan 10) or calculating the yearly average, 
+# indicates that almost none of the data variation is learned by our model?
+
+# choosing a specific day
+# calculating the yearly average
+# ANSWER --> choosing a specific day
+
+
+# Which graph, choosing a specific day (Jan 10) or calculating the yearly average, 
+# supports more the claim that global warming is leading to an increase in temperature?
+
+# choosing a specific day
+# calculating the yearly average
+# ANSWER --> calculating the yearly average
